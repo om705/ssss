5ae180418c6c795390eb645c5940111a7a4772d0
om705 <61368509+om705@users.noreply.github.com>
2025-11-08 21:51:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "api id"
-api_hash = "api hadh"
-bot_token = "bot token"
-auth_users = [7890781002]
-sudo_users = [7890781002]
+api_id = "22114233"
+api_hash = "d7abcec5c967414fadb1d438fa05ebea"
+bot_token = "8393895951:AAFX8W94ZFnG-C0HPtwxp-1rHd-sKXZu89A"
+auth_users = [1403488629]
+sudo_users = [1403488629]
